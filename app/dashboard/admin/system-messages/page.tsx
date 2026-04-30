5641707e3fdd88c79af70b9a77fb2d328d84b6c0
mennobd <30756836+mennobd@users.noreply.github.com>
2026-04-30 09:26:22 +0200

    Update page.tsx

--- app/dashboard/admin/system-messages/page.tsx
+++ app/dashboard/admin/system-messages/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { prisma } from "@/lib/db";
 import { getRequiredMutationUser } from "@/lib/auth";
 import SystemMessageForm from "@/components/SystemMessageForm";
